1df520e3ce86055642721cfbd8752f50861548fd
jessica9z7 <jessicakusmierz11@gmail.com>
2026-05-05 11:23:50 -0500

    Delete promblem 3.py

--- promblem 3.py
+++
@@ -1,9 +0,0 @@
-#Jessica Kusmierz
-#10/31/2025
-#Problem 3
-
-import random
-print("Random Day of the Week")
-days_of_the_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-random_day =random.choice(days_of_the_week)
-print(random.choice(days_of_the_week))
